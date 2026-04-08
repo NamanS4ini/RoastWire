c57f4773c49891f2dbc5fe98f4297d9bd6e7184f
Naman Saini <namansaini464@gmail.com>
2026-04-08 21:28:43 +0530

    Fixed name case

--- components/navigation/NavigationAction.tsx
+++ components/navigation/NavigationAction.tsx
@@ -3,7 +3,7 @@ import ActionTooltips from '../ActionTooltips'
 import { ModeToggle } from '../ui/themeButton'
 const NavigationAction = () => {
     return (
-        <div className='flex flex-col items-center h-full'>
+        <div className='flex flex-col items-center'>
             <ActionTooltips label='Add a Server' side='right' align='center'>
                 <button className='group flex items-center cursor-pointer'>
                     <div className='flex mx-3 h-12 w-12 rounded-[24px] group-hover:rounded-xl transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500 dark:group-hover:bg-emerald-500'>
@@ -11,16 +11,6 @@ const NavigationAction = () => {
                     </div>
                 </button>
             </ActionTooltips>
-
-
-
-
-
-
-            
-            <button className='pt-5'>
-                <ModeToggle />
-            </button>
         </div>
     )
 }
--- components/navigation/NavigationSidebar.tsx
+++ components/navigation/NavigationSidebar.tsx
@@ -1,8 +1,8 @@
 import { currentProfile } from '@/lib/current-user'
 import { redirect } from 'next/navigation';
-import Image from 'next/image';
 import { db } from '@/lib/db';
 import NavigationAction from './NavigationAction';
+import { Separator } from '../ui/separator';
 
 const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -19,8 +19,14 @@ const NavigationSidebar = async () => {
     },
   });
   return (
-    <div className='space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3'>
+    <div className='space-y-4 flex flex-col items-center h-full text-primary dark:bg-[#1E1F22] py-3'>
       <NavigationAction />
+      <Separator className='h-0.5 bg-zinc-300 dark:bg-zinc-700 rounded-md w-10!' />
+      {server.map((server) => (
+        <div key={server.id} className='relative group flex items-center cursor-pointer'>
+        </div>
+      ))}
+
     </div>
   )
 }
