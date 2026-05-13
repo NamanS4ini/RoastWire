4f5bf551e663e2f2b3d76320b2d114076b38d0d8
Naman Saini <namansaini464@gmail.com>
2026-05-13 06:17:10 +0530

    Fixed params type errors

--- app/(main)/(routes)/servers/[id]/layout.tsx
+++ app/(main)/(routes)/servers/[id]/layout.tsx
@@ -5,11 +5,11 @@ import { RedirectToSignIn } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
 import React from 'react'
 
-const layout = async ({ children, params }: { children: React.ReactNode, params: { id: string } }) => {
+const layout = async ({ children, params }: { children: React.ReactNode, params: Promise<{ id: string }> }) => {
     const profile = await currentProfile()
-    const id = (await params).id
-if (!profile) {
-    return <RedirectToSignIn />;
+    const { id } = await params
+    if (!profile) {
+        return <RedirectToSignIn />;
     }
     const server = await db.server.findUnique({
         where: {
@@ -25,16 +25,16 @@ if (!profile) {
         return redirect("/");
     }
 
-  return (
-      <div className="h-full">
-          <div className='hidden md:flex h-full w-60 z-20 flex-col inset-y-0 fixed'>
-              <ServerSidebar serverId={id} />
-          </div>
-          <main className='h-full md:pl-60'>
-          {children}
-          </main>
-      </div>
-  )
+    return (
+        <div className="h-full">
+            <div className='hidden md:flex h-full w-60 z-20 flex-col inset-y-0 fixed'>
+                <ServerSidebar serverId={id} />
+            </div>
+            <main className='h-full md:pl-60'>
+                {children}
+            </main>
+        </div>
+    )
 }
 
 export default layout
